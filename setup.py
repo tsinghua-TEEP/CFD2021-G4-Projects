135dfd98c477ff88907adc24d54532ff781ca78d
Tci Gravifer Fang <44160838+Gravifer@users.noreply.github.com>
2021-04-29 12:16:06 +0800

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Setup script of the project.
+
+Not yet implemented. The following is an example of a python module docstring.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
